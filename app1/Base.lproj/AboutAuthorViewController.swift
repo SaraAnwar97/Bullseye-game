a77a8424720e1433f938736c00d66e6c5b9dc4f9
Sara Anwar <saraanwar97@gmail.com>
2021-04-27 02:35:12 +0200

    Done

---
+++ app1/Base.lproj/AboutAuthorViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutAuthorViewController.swift
+//  app1
+//
+//  Created by Sara Anwar on 7/5/19.
+//  Copyright © 2019 Sara Anwar. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+
+
+}
---
+++ app1/Base.lproj/AboutViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AboutViewController.swift
+//  app1
+//
+//  Created by Sara Anwar on 7/5/19.
+//  Copyright © 2019 Sara Anwar. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView : WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html"){
+            let url = URL (fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
